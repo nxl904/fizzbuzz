cc4c122c90dad8a90f29852fd1f4738cbb156c13
Nic <nlogalbo@gmail.com>
2017-02-08 19:35:34 -0600

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,17 @@
+
+n = int(input("Select first number of range: "))
+n2 = int(input("Select second number of range: "))
+r = range(n,n2)
+
+def FizzBuzz():
+	for a in r: 
+		if a%5 == 0 and a%3==0: 
+			print ("FizzBuzz")
+		elif a%3 == 0: 
+			print ("Buzz")
+		elif a%5 == 0: 
+		    print ("Fizz")
+		else: 
+		    print(a)
+
+FizzBuzz()
